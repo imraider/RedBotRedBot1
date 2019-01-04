231dcdc3a3f81914af785bf7ace8975eae548201
imraider <45240051+imraider@users.noreply.github.com>
2019-01-04 16:12:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,7 +128,7 @@ client.on('message', message => {
         .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
         .setTitle(`:small_orange_diamond: اضغط هنا `)
         .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=466256531998900246&permissions=8&scope=bot`)
-        .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
+        .setThumbnail(" https://discordapp.com/oauth2/authorize?client_id=530740187156840451&permissions=8&scope=bot")        
      message.channel.sendEmbed(embed);
        }
    });
